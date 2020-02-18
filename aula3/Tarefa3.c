6ded3b39d2af1fb86f5fb3118361708b0b020467
Vitor Carvalho <vitoracarvalho@gmail.com>
2020-02-18 19:15:07 +0000

    Works for every hexagon size

---
+++ aula3/Tarefa3.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+
+void hex(int n){
+	int var = n - 4;
+	int a = n-1, algo, j=0;
+	char caracter=' ';
+	int limit = (n * 2 + a);
+	//First line
+	for(int i=0;i<2;i++, caracter+=3){
+		for(int j=0;j<n;j++){
+			printf("%c", caracter);
+		}
+	}
+	printf("\n"); caracter=' '; // reset char to space and print newline
+	//Ascending lines
+	for(int linha=1, middleSpaces=n, initialSpaces=(n-1);linha<n;linha++, initialSpaces--, middleSpaces+=2){
+		for(int j=0;j<initialSpaces;j++){
+			printf(" ");
+		}
+		printf("#");
+		for(int i=0;i<middleSpaces;i++){
+			printf(" ");
+		}
+		printf("#");
+		printf("\n");
+	}
+	//Descending lines
+	for(int linha=1, middleSpaces=(n*2)-2, initialSpaces=2;linha<n-1;linha++, initialSpaces++, middleSpaces-=2){
+		for(int j=0;j<initialSpaces;j++){
+			printf(" ");
+		}
+		printf("#");
+		for(int i=0;i<middleSpaces+var;i++){
+			printf(" ");
+		}
+		printf("#");
+		printf("\n");
+	}
+	//Last line
+	for(int i=0;i<2;i++, caracter+=3){
+		for(int j=0;j<n;j++){
+			printf("%c", caracter);
+		}
+	}
+	printf("\n");
+}
+
+
+
+int main(){
+	int size=0;
+	printf("Insira o tamanho do hexagono desejado\n");
+	scanf("%d", &size);
+	hex(size);
+	return 0;
+}
