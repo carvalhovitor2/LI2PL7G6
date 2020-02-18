6ff3d729bfb7b8b5415556d6f768adc34cd1698c
Vitor Carvalho <vitoracarvalho@gmail.com>
2020-02-18 14:40:15 +0000

    Using ex4-ficha1-PI as base

---
+++ aula3/Tarefa1.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+#include <string.h>
+
+void horizontal(int size){
+	int i, j=0, k=0;
+	char c1='A', param='A';
+	for( i = size;i>0;i--){
+		while( j <= (i - 2)){
+			printf("  ");
+			j++;
+		}
+		while( k <= (size - i)){
+			printf(" %c", c1);
+			k++;
+			c1++;
+		}
+		c1-=2;
+		k = 0;
+		while( k <= (size - (i+1))){
+			printf(" %c", c1);
+			k++;
+			c1--;
+		}
+
+		printf("\n");
+		j = 0;
+		k = 0;
+		c1='A';
+	}
+}
+
+
+int main(){
+	int i=4;
+	horizontal(i);
+	return 0;
+}
