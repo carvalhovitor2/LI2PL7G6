e3b6666001b631efc116fc1f1f784ea5150bc7c3
Vitor Carvalho <vitoracarvalho@gmail.com>
2020-02-26 19:07:53 +0000

    Cleaning code and some comments added

--- aula4/main.c
+++ aula4/main.c
@@ -25,17 +25,17 @@ void roda_esq(int *arr, int dim, int shifter){
 
 
 int main(){
-	int dim=0, shifter=0, *ptr, tarefa, argumento;
+	int dim=0, *ptr, tarefa, argumento;
 	printf("Escolha o numero da tarefa:\n");
 	scanf("%d", &tarefa);
 	printf("Insira a dimensao do array:\n");
 	scanf("%d", &dim);
+	//Allocating vector needed space in heap
 	ptr = (int*) malloc(dim * sizeof(int));
 	for(int elem=0;elem<dim;elem++){
 		printf("Elem %d:\n", elem+1);
 		scanf("%d", ptr+elem);
 	}
-	shifter = shifter%dim;
 	switch(tarefa){
 		case 1: printf("Insira o index:\n");
 			scanf("%d", &argumento);
@@ -43,6 +43,7 @@ int main(){
 			break;
 		case 2: printf("Insira o shifter:\n");
 			scanf("%d", &argumento);
+			argumento %= dim;
 			roda_esq(ptr, dim, argumento);
 		default:
 			break;
