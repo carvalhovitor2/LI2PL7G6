f3324de742e6920496c3702037affab0f89cba22
Vitor Carvalho <vitoracarvalho@gmail.com>
2020-03-08 23:07:29 +0000

    Removing functions from main game and placing them in functions.c and functions.h

--- projeto/main_game.c
+++ projeto/main_game.c
@@ -1,43 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "game.h"
-
-void print_tabuleiro(char matriz[8][8]){
-	for(int linhas = 0; linhas < 8; linhas++){
-		for(int colunas=0; colunas < 8; colunas++){
-			printf("%c", matriz[linhas][colunas]);	
-		}
-		printf("\n");
-	}
-
-}
-
-
-void gr(char tabuleiro[8][8]){
-	FILE *file;
-	file = fopen("estado_tabuleiro", "ab+");
-	for(int i=0; i<8; ++i)fwrite(tabuleiro[i], sizeof(tabuleiro[i][0]), 8, file);
-}
-
-//void ler(){
-//	FILE *file;
-//	unsigned char algo1;
-//	file = fopen("estado_tabuleiro", "ab+");
-//	for(int i=0; i<8; ++i)fread(tabuleiro, sizeof
-//	
-//}
-
-void preenche_inicial(char tabuleiro[8][8]){
-	for(int linhas = 0; linhas < 8 ; linhas++){
-		for(int colunas=0; colunas < 8 ; colunas++){
-			if( linhas == 0 && colunas == 7)tabuleiro[linhas][colunas]='2';
-			else if ( linhas == 7 && colunas == 0)tabuleiro[linhas][colunas]='1';
-			else tabuleiro[linhas][colunas]='.';
-
-		}
-	}
-}
-
+#include "functions.h"
 
 int main(){
         char ch;
