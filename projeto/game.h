03d73189dc68cb77fc77217ee4c6526e3bccc888
Vitor Carvalho <vitoracarvalho@gmail.com>
2020-03-08 21:47:01 +0000

    A atualizar modulo de maneira com que todas as estruturas de dados fiquem neste ficheiro

--- projeto/game.h
+++ projeto/game.h
@@ -1,9 +1,27 @@
 #ifndef HEADER_FILE
 #define HEADER_FILE
 
-typedef struct coordenada {
-	char linha;
+typedef enum {VAZIO, BRANCA, PRETA} CASA;
+
+
+typedef struct {
 	int coluna;
-}coordenada;
+	int linha;
+} COORDENADA;
+
+typedef struct {
+	COORDENADA jogador1;
+	COORDENADA jogador2;
+} JOGADA;
+
+typedef JOGADA JOGADAS[32];
+
+typedef struct {
+	CASA tab[8][8];
+	JOGADAS jogadas;
+	int num_jogadas;
+	int jogador_atual;
+} ESTADO;
+
 
 #endif
