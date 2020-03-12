101a7ead25b747276ab0081ab2cc8957c70e8f3c
Vitor Carvalho <vitoracarvalho@gmail.com>
2020-03-12 10:44:50 +0000

    Adicionar verificacao de coordenada valida e a modularizar as funcoes

--- projeto/logic/logic.c
+++ projeto/logic/logic.c
@@ -6,8 +6,10 @@
 int jogar(ESTADO *e, COORDENADA c){
     int column = c.coluna;
     int line = c.linha;
+    //Checks if it's a valid move. Returns false if it's not a valid move.
+    if ( line < 1 || line > 8 || column < 1 || column > 8 ) return 0; 
     e-> tab[line-1][column-1] = BRANCA;
-    if (e-> jogador_atual == 1) {
+    if (getCurrentPlayer(e) == 1) {
         e-> jogadas[e-> num_jogadas].jogador1 = c;
         e-> jogador_atual = 2;
     }
@@ -17,8 +19,6 @@ int jogar(ESTADO *e, COORDENADA c){
         e-> jogador_atual = 1;
         e-> num_jogadas++;
     }
-
-    printf("jogar %d %d\n", line, column);
     return 1;
 }
 
