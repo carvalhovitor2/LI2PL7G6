824f8f1e6033dfbcde350378c34e2f9dd2a98679
Vitor Carvalho <vitoracarvalho@gmail.com>
2020-03-12 10:08:21 +0000

    Adjusting indentation

--- projeto/main_game.c
+++ projeto/main_game.c
@@ -3,9 +3,9 @@
 #include "interface/interface.h"
 
 int main(){
-    COORDENADA c = {2,5};
+	COORDENADA c = {2,5};
 	ESTADO *e=inicializar_estado();
-    jogar(e ,c);
+	jogar(e ,c);
 	mostrar_tabuleiro(e);
 	return 0;
 }
