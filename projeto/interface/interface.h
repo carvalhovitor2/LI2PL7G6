208f6c738f7476c07f19b2c760b053f963aa01b4
Pmrf555 <61868738+Pmrf555@users.noreply.github.com>
2020-03-20 14:22:56 +0000

    Documentação.

--- projeto/interface/interface.h
+++ projeto/interface/interface.h
@@ -3,8 +3,15 @@
 #include <stdlib.h>
 #include "../logic/logic.h"
 
-
+/**
+\brief Função que imprime o tabuleiro
+@param e Apontador para o estado
+@param whereToPrint Apontador para guardar o estado.
+*/
 void mostrar_tabuleiro(FILE *whereToPrint, ESTADO *e);
+
+/**
+\brief Fucção que lê e executa o comando dado
+@param e Apontador para o estado
+*/
 int interpretador(ESTADO *e);
-
-
--- projeto/logic/logic.h
+++ projeto/logic/logic.h
@@ -1,5 +1,16 @@
 //This file is supposed to store the declarations of the logic functions used in our project
 #include "../data/data.h"
 
+/**
+\brief Função que faz a jogada e atualiza o estado com ajuda das funções dos dados
+@param e Apontador para o estado
+@param c A coordenada
+*/
 int jogar(ESTADO *e, COORDENADA c);
+
+/**
+\brief Função que dado a coordenada vê se a jogada é válida e se não for avisa o jogador
+@param e Apontador para o estado
+@param c A coordenada
+*/
 int jogadaValida(ESTADO *e, COORDENADA c);
