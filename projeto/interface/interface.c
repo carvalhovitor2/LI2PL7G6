293ada645a8ec25ae798036f310e0002e7bc85f2
Vitor Carvalho <vitoracarvalho@gmail.com>
2020-04-16 22:19:21 +0100

    AGORA SIM FUNCIONA ESTA BOSTA -- Leitura de todos os movs completos - ate mesmo quando somente 1 jogador fez a jogada

--- projeto/interface/interface.c
+++ projeto/interface/interface.c
@@ -267,49 +267,52 @@ void ler(char *fileName, ESTADO *e){
                 break;
 
         }
-        if (buffer == 8){
-            printf("jogada = %c -- p1 = %c%c p2 = %c%c\n", jogada, p1Linha, p1Coluna, p2Linha, p2Coluna);
+        if (buffer == 6){
             	coord->linha = p1Linha - 'a';
             	coord->coluna = p1Coluna - '1';
-            	coord1->linha = p2Linha - 'a';
-            	coord1->coluna = p2Coluna - '1';
 		e->jogadas[jogada-'1'].jogador1 = *coord;
-		e->jogadas[jogada-'1'].jogador2 = *coord1;
-		incrJogada(e);
-            //Discards newline
-            fscanf(file, " ");
         }
+	if (buffer == 8){
+                coord1->linha = p2Linha - 'a';
+                coord1->coluna = p2Coluna - '1';
+                e->jogadas[jogada-'1'].jogador2 = *coord1;
+		incrJogada(e);
+		//Discards newline
+                fscanf(file, " ");
+	}
     }
     //Reads other 20 movs
         for(int buffer = 0, linha = 0;linha < 200 && (fscanf(file, "%c", c) == 1); buffer++, linha++, buffer %=9){
-        switch(buffer){
-            case 1: jogada = *c;
-                break;
-            case 4: p1Linha = *c;
-                break;
-            case 5: p1Coluna = *c;
-                break;
-            case 7: p2Linha = *c;
-                break;
-            case 8: p2Coluna = *c;
-                break;
-            default:
-                break;
-        }
-        if (buffer == 8){
-            printf("jogada = %c -- p1 = %c%c p2 = %c%c\n", jogada, p1Linha, p1Coluna, p2Linha, p2Coluna);
-                coord->linha = p1Linha - 'a';
-                coord->coluna = p1Coluna - '1';
-                coord1->linha = p2Linha - 'a';
-                coord1->coluna = p2Coluna - '1';
-                e->jogadas[jogada-'1' + 10].jogador1 = *coord;
-                e->jogadas[jogada-'1' + 10].jogador2 = *coord1;
-                incrJogada(e);
-            //Discards newline
-            fscanf(file, " ");
-        }
-    }
-
+	        switch(buffer){
+	            case 1: jogada = *c;
+	                break;
+	            case 4: p1Linha = *c;
+	                break;
+	            case 5: p1Coluna = *c;
+	                break;
+	            case 7: p2Linha = *c;
+	                break;
+	            case 8: p2Coluna = *c;
+	                break;
+	            default:
+	                break;
+	        }
+	        if (buffer == 6){
+	                coord->linha = p1Linha - 'a';
+	                coord->coluna = p1Coluna - '1'; 
+	                e->jogadas[jogada-'1'+10].jogador1 = *coord;
+			changePlayer(e);
+	        }
+	        if (buffer == 8){
+	                coord1->linha = p2Linha - 'a';
+	                coord1->coluna = p2Coluna - '1';
+	                e->jogadas[jogada-'1'+10].jogador2 = *coord1;
+			changePlayer(e);
+	                incrJogada(e);
+	                //Discards newline
+	                fscanf(file, " ");
+	        }
+	    }
 }
 
 
