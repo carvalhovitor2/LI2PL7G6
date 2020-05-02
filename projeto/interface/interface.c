4dec954615c646f03e1c8ccc07ef5a4a3232e613
Vitor Carvalho <vitoracarvalho@gmail.com>
2020-05-02 20:36:44 +0100

    Optimizacao funcao ler

--- projeto/interface/interface.c
+++ projeto/interface/interface.c
@@ -283,14 +283,11 @@ void ler(char *fileName, ESTADO *e){
 
     //Auxiliar char to translate chars to integers
     char *c=malloc(sizeof(char));
-    //Iterates through lines
+
+    //Iterates trough board lines
     for(int linha = 0; linha < 8; linha++){
-
-        //Please note that scanf functions read something and moves the reading pointer to the next thing to be read.
-        //That is why you can iterate it like this
-        //Iterates through columns
+        //Iterates through board columns
         for(int coluna = 0; coluna < 8; coluna++){
-
             //Reads a piece and stores it in game state
             int try = fscanf(file, "%c ", c);
 	    if(try != 1)printf("could read char\n");
@@ -300,11 +297,10 @@ void ler(char *fileName, ESTADO *e){
         }
     }
 
-    //Discarding number line of the board (1, 2, 3 ...)
-    //fscanf(file, "1 2 3 4 5 6 7 8 ");
     char jogada='a', p1Linha='a', p1Coluna='a', p2Linha='a', p2Coluna='a';
-    //Reads first 10 movs
-    for(int buffer = 0, linha = 0;linha < 81 && (fscanf(file, "%c", c) == 1); buffer++, linha++, buffer %=9){
+
+    //Reads movs
+    for(int buffer = 0, linha = 0;linha < 281 && (fscanf(file, "%c", c) == 1); buffer++, linha++, buffer %=9){
         switch(buffer){
             case 1: jogada = *c;
                 break;
@@ -324,52 +320,25 @@ void ler(char *fileName, ESTADO *e){
             	coord->linha = p1Linha - 'a';
 		changePlayer(e);
             	coord->coluna = p1Coluna - '1';
-		e->jogadas[jogada-'1'].jogador1 = *coord;
+	        if(linha > 81)
+			e->jogadas[jogada-'1'+10].jogador1 = *coord;
+		else
+			e->jogadas[jogada-'1'].jogador1 = *coord;
         }
 	if (buffer == 8){
                 coord1->linha = p2Linha - 'a';
 		changePlayer(e);
                 coord1->coluna = p2Coluna - '1';
-                e->jogadas[jogada-'1'].jogador2 = *coord1;
+	        if(linha > 81)
+			e->jogadas[jogada-'1'+10].jogador2 = *coord1;
+		else
+		       	e->jogadas[jogada-'1'].jogador2 = *coord1;
 		incrJogada(e);
 		//Discards newline
                 int try = fscanf(file, " ");
 		if (try != 1 && try != 0)printf("Couldnt read char");
 	}
     }
-    //Reads other 20 movs
-        for(int buffer = 0, linha = 0;linha < 200 && (fscanf(file, "%c", c) == 1); buffer++, linha++, buffer %=9){
-	        switch(buffer){
-	            case 1: jogada = *c;
-	                break;
-	            case 4: p1Linha = *c;
-	                break;
-	            case 5: p1Coluna = *c;
-	                break;
-	            case 7: p2Linha = *c;
-	                break;
-	            case 8: p2Coluna = *c;
-	                break;
-	            default:
-	                break;
-	        }
-	        if (buffer == 6){
-	                coord->linha = p1Linha - 'a';
-	                coord->coluna = p1Coluna - '1'; 
-	                e->jogadas[jogada-'1'+10].jogador1 = *coord;
-			changePlayer(e);
-	        }
-	        if (buffer == 8){
-	                coord1->linha = p2Linha - 'a';
-	                coord1->coluna = p2Coluna - '1';
-	                e->jogadas[jogada-'1'+10].jogador2 = *coord1;
-	                incrJogada(e);
-			changePlayer(e);
-	                //Discards newline
-	                int try = fscanf(file, " ");
-			if(try != 1)printf("Couldnt read char - 3\n");
-	        }
-	    }
 }
 
 
